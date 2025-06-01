d92da62ca48c4cc654081cde7729a3d2ae08fd19
Ivan Ordonez <ivordalv@gmail.com>
2025-06-01 11:23:33 +0200

    webpack arreglo

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,6 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true, // limpia dist/ antes de cada build
   },
   module: {
     rules: [
@@ -14,11 +16,23 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      // si necesitas procesar otros assets (imágenes, fonts...), agrégalos aquí
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'JS'), // carpeta que quieres copiar
+          to: path.resolve(__dirname, 'dist/JS'), // destino dentro de dist
+          globOptions: {
+            ignore: ['**/*.DS_Store'],
+          },
+        },
+      ],
     }),
   ],
   devServer: {
