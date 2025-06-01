51b3f9ba99608942cf8c23fd199afe03a843566b
Ivan Ordonez <ivordalv@gmail.com>
2025-06-01 12:27:38 +0200

    otro commit

--- JS/services/supabase.js
+++ JS/services/supabase.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e588606e5742fd8e97779500ceab68034c7e48cf24e169191d960a4ca51ff722
-size 809
+oid sha256:7700d74234e442ef4e66a09306d64731c2fe50cffd58041c19b73cb23de9023b
+size 828
